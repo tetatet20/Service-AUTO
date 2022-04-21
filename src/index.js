d3b2c882e97f011da5ce1bc4042153d152949df0
tetatet20 <romanseregicev@gmail.com>
2022-04-21 16:42:29 +0300

    2

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ sendButton.addEventListener('click', function () {
     const numberValue = number.value
     const emailValue = email.value
     const date = new Date().toLocaleDateString('ru-Ru')
+    if (numberValue  === "") {
+        alert('Заполните поля');
+    }
+    else {fetch(`https://api.telegram.org/bot5347573628:AAGlHUQzNDeAaC5WSToxc5cXpFYProrrqoU/sendMessage?chat_id=-1001776896462&text=${date}%0AИмя%20%2D%20${nameValue}%0AНомер%20%2D%20${numberValue}%0AEmail%20%2D%20${emailValue}`)
 
-    fetch(`https://api.telegram.org/bot5347573628:AAGlHUQzNDeAaC5WSToxc5cXpFYProrrqoU/sendMessage?chat_id=-1001776896462&text=Имя%20%2D%20${nameValue}%0AНомер%20%2D%20${numberValue}%0AEmail%20%2D%20${emailValue}`)
+    sendButton.classList.add('call-me-back-active')}
+    
+    // if ((sendButton.classList.contains('active')))
+    
 })
