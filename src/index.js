dbdfde7139ec4386e5d7e3997b7506b5e0a1b29e
tetatet20 <romanseregicev@gmail.com>
2022-04-28 22:47:15 +0300

    исправил отображение в сафари

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ sendButton.addEventListener('click', function () {
 	const date = new Date().toLocaleDateString('ru-Ru')
 	if (numberValue && nameValue && emailValue ) {
 		fetch(
-			`https://api.telegram.org/bot5347573628:AAGlHUQzNDeAaC5WSToxc5cXpFYProrrqoU/sendMessage?chat_id=-1001776896462&text=${date}%0AИмя%20%2D%20${nameValue}%0AНомер%20%2D%20${numberValue}%0AEmail%20%2D%20${emailValue}`
+			`https://api.telegram.org/bot5347573628:AAGlHUQzNDeAaC5WSToxc5cXpFYProrrqoU/sendMessage?chat_id=-1001776896462&text=${date}%0AИмя%20%2D%20${nameValue}%0AНомер%20%2D%20${numberValue}%0AТранспорт%20%2D%20${emailValue}`
 		)
 		sendButton.innerHTML = "Спасибо за обращение"
 		sendButton.classList.add('call-me-back-active')
